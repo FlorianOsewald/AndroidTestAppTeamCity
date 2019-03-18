e766578100c037ec0cdfc3ecb8c12896e4e313a0
FlorianOsewald <48680939+FlorianOsewald@users.noreply.github.com>
2019-03-18 13:43:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    textView.setText("Goodbye!");
+                else
+                    textView.setText("Hello World!");
+            }
+        });
+    }
+
+}
